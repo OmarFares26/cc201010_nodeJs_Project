49e281f2e5e1edfd6c70cdbab7409ba941e0d44b
cc201013 <cc201013.fhstp.ac.at>
2021-04-08 12:41:09 +0200

    Commit every thing we coverd in the session

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors());
+
+
+app.use(fileUpload({
+    createParentPath: true
+}))
+
+
+//app.get('/', (req, res) => {
+  //  res.send('Hello World!');
+//});
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
+
+
+
+
 app.listen(port, () => {
     console.log(`Example app listening at
 http://localhost:${port}`);
