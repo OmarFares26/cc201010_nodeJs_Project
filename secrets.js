4fd5cb58e5f0c4571e477b593e968133cabd5f45
Omar Fares <90091619+OmarFares26@users.noreply.github.com>
2021-11-30 13:37:59 +0100

    Delete secrets.js

--- secrets.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    dbPassword: 'wa>Gd&M5',
-    access_token_secret: '33c31a4782d32f88acec4795e87ed9cf6298bb22a10f3e7e884835c58eaa23b32fd8fc9ba0facb55e16e275c0154aaf9294312f60bd3a181eee8c6848fac34ce'
-};
